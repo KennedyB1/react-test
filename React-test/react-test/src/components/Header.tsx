cb3d309426f3cd7090426c60f44d70ac618cbd60
KennedyB1 <1994_simon@live.se>
2023-08-05 00:41:06 +0200

    update

--- React-test/react-test/src/components/Header.tsx
+++ React-test/react-test/src/components/Header.tsx
@@ -3,18 +3,19 @@ export const Header = () => {
 
     return (
         <header>
+            
             <div className="logo">
                 {/* Replace 'logo.png' with the path to your logo image */}
                 <img src="logo.png" alt="Logo" />
             </div>
-            <div className="buttons">
-                <button>Random</button>
+            
+                <button>Random chant</button>
                 <input type="text" placeholder="Search for team or chant" />
                 <button>Battle</button>
 
-            </div>
 
             <div className="logout"><button>Log in</button></div>
+            
         </header>
     );
 
