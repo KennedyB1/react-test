763e3a6c2d850f49ba8141b59e36b78c257c8f36
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-08-05 22:12:00 +0200

    Update

--- React-test/react-test/src/App.tsx
+++ React-test/react-test/src/App.tsx
@@ -1,34 +1,36 @@
-/* import { useEffect } from 'react'
+import { useEffect } from 'react'
 
 
-import axios from 'axios'; */
+import axios from 'axios';
 import { RouterProvider } from "react-router-dom";
 import './App.css'
 import { Header } from './components/Header'
 import { router } from "./components/Router";
+import { Footer } from "./components/Footer";
 // import { IPersson } from './models/IPersson';
 
 
 function App() {
-  /*
-    useEffect(() => {
-      const getData = async () => {
-        const respons = await axios.get("https://swapi.dev/api/people/1/");
-        console.log(respons.data);
-      }
-  
-      getData();
-    });
-    
-    <RouterProvider router={router} />
-  return <div className="app">
-    <Header />
-    
-  </div>*/
+
+  // const [name, setName] = useState('');
+
+  useEffect(() => {
+    const getData = async () => {
+      const respons = await axios.get("https://transfermarkt-api.vercel.app/clubs/search/Hammarby");
+      // setName(respons.data.name);
+      console.log(respons)
+    }
+
+    getData();
+
+  });
 
   return <><RouterProvider router={router} />
 
     <Header />
+    <Footer></Footer>
+
+
   </>
 
 
--- React-test/react-test/src/components/hello.tsx
+++ React-test/react-test/src/components/hello.tsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 export const Hellow = () => {
     //  const age: number = 12;
 
     const numbers = [1, 2, 22, 4];
 
-    return <ul>
+    return <div className="side-chant"><h2>Chants</h2><ul>
         {
-            numbers.map((i, index) => <li key={index}>{i}</li>)
+            numbers.map((i, index) => <li key={index}><Link to={`/starwars/${i}`}>{i}</Link></li>)
         }
 
     </ul>
-
+    </div>
 
 }
