1395f39b8d54d9454463075779bb49806f663b5c
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-08-06 16:28:40 +0200

    a

--- React-test/react-test/src/App.tsx
+++ React-test/react-test/src/App.tsx
@@ -1,45 +1,47 @@
-import { useEffect } from 'react';
-import axios from 'axios';
-import { RouterProvider } from 'react-router-dom';
-import './App.css';
-import { Header } from './components/Header';
-import { router } from './components/Router';
-import { Footer } from './components/Footer';
+import { useEffect } from 'react'
 
-interface Club {
-  id: string;
-  url: string;
-  name: string;
-  country: string;
-  squad: string;
-  marketValue: string;
-}
+
+import axios from 'axios';
+import { RouterProvider } from "react-router-dom";
+import './App.css'
+import { Header } from './components/Header'
+import { router } from "./components/Router";
+import { Footer } from "./components/Footer";
+// import { IPersson } from './models/IPersson';
+// https://transfermarkt-api.vercel.app/docs#/
 
 function App() {
+
+  // const [name, setName] = useState('');
+
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await axios.get<Club[]>(
-          'https://transfermarkt-api.vercel.app/clubs/search/Hammarby'
+        const response = await axios.get(
+          'https://transfermarkt-api.vercel.app/clubs/search/hammarby%20if'
         );
-        const data = response.data;
-        const names = data.map((club: Club) => club.name);
-        console.log(names);
+        console.log(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
     getData();
-  }, []);
 
-  return (
-    <>
-      <RouterProvider router={router} />
-      <Header />
-      <Footer />
-    </>
-  );
+  });
+
+  return <><RouterProvider router={router} />
+
+    <Header />
+    <Footer></Footer>
+
+
+  </>
+
+
+
+
 }
 
-export default App;
+export default App
+
