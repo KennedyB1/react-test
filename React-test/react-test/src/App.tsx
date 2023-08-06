290570cb18a6d47454a69128a2cd5b5f0331b089
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-08-06 16:24:45 +0200

    test

--- React-test/react-test/src/App.tsx
+++ React-test/react-test/src/App.tsx
@@ -1,42 +1,45 @@
-import { useEffect } from 'react'
-
-
+import { useEffect } from 'react';
 import axios from 'axios';
-import { RouterProvider } from "react-router-dom";
-import './App.css'
-import { Header } from './components/Header'
-import { router } from "./components/Router";
-import { Footer } from "./components/Footer";
-// import { IPersson } from './models/IPersson';
-// https://transfermarkt-api.vercel.app/docs#/
+import { RouterProvider } from 'react-router-dom';
+import './App.css';
+import { Header } from './components/Header';
+import { router } from './components/Router';
+import { Footer } from './components/Footer';
 
-function App() {
-
-  // const [name, setName] = useState('');
-
-  useEffect(() => {
-    const getData = async () => {
-      const respons = await axios.get("https://transfermarkt-api.vercel.app/clubs/search/Hammarby");
-      // setName(respons.data.name);
-      console.log(respons)
-    }
-
-    getData();
-
-  });
-
-  return <><RouterProvider router={router} />
-
-    <Header />
-    <Footer></Footer>
-
-
-  </>
-
-
-
-
+interface Club {
+  id: string;
+  url: string;
+  name: string;
+  country: string;
+  squad: string;
+  marketValue: string;
 }
 
-export default App
+function App() {
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get<Club[]>(
+          'https://transfermarkt-api.vercel.app/clubs/search/Hammarby'
+        );
+        const data = response.data;
+        const names = data.map((club: Club) => club.name);
+        console.log(names);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
+    getData();
+  }, []);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
