4d03ad325164ff7f77e216c8d3cce3b6876dfa36
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-09-16 15:29:49 +0200

    Update

--- React-test/react-test/src/components/sideBar.tsx
+++ React-test/react-test/src/components/sideBar.tsx
@@ -1,61 +1,8 @@
-//import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-
-
 import '../style/sidebar.css';
  
 
-
 export const Hellow = () => {
 
-    //https://www.npmjs.com/package/react-pro-sidebar?activeTab=readme
-/*
-    return (
-        <>
-        
-          <Sidebar className="app">
-        <p>Chants</p>
-            <Menu>
-              <MenuItem> Dashboard </MenuItem>
-              <MenuItem> Invoices </MenuItem>
-              <SubMenu label="Charts">
-                <MenuItem> Timeline Chart </MenuItem>
-                <MenuItem> Bubble Chart </MenuItem>
-              </SubMenu>
-              <SubMenu label="Wallets">
-                <MenuItem>Current Wallet</MenuItem>
-                <MenuItem>Savings Wallet</MenuItem>
-              </SubMenu>
-              <MenuItem> Transactions </MenuItem>
-              <SubMenu label="Settings">
-                <MenuItem> Account </MenuItem>
-                <MenuItem> Privacy </MenuItem>
-                <MenuItem> Notifications </MenuItem>
-              </SubMenu>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-              <MenuItem> Logout </MenuItem>
-            </Menu>
-          </Sidebar>
-          
-        </>
-      );
-      */
-
-
-
-
-  
   return (
     <aside>
      
